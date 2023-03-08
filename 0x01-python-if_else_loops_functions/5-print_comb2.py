3e160ce3b94cc716d2eb12e26239fe17891f559a
Dadson Mweya <mweyadadson@gmail.com>
2023-03-08 18:06:42 +0300

    python-if_else_loops_functions

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
-for i in range(0, 99):
-    print("{:02d}".format(i), end=', ')
-print("{:02d}".format(i + 1))
+for digit in range(0, 100):
+    if digit == 99:
+        print("{}".format(digit))
+    else:
+        print("{:02}".format(digit), end=', ')
