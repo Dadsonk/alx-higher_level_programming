09a3870de6625de17b8c25c9ede52341475c9a13
Dadson Mweya <mweyadadson@gmail.com>
2023-04-23 08:03:03 +0300

    python-almost_a_circle

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -2,15 +2,12 @@
 # square.py
 
 
-
 """Defines a square class."""
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
     """Represent a square."""
-
-
 
     def __init__(self, size, x=0, y=0, id=None):
         """Initialize a new Square.
@@ -22,21 +19,15 @@ class Square(Rectangle):
         """
         super().__init__(size, size, x, y, id)
 
-
-
     @property
     def size(self):
         """Get/set the size of the Square."""
         return self.width
 
-
-
     @size.setter
     def size(self, value):
         self.width = value
         self.height = value
-
-
 
     def update(self, *args, **kwargs):
         """Update the Square.
@@ -78,8 +69,6 @@ class Square(Rectangle):
                 elif k == "y":
                     self.y = v
 
-
-
     def to_dictionary(self):
         """Return the dictionary representation of the Square."""
         return {
@@ -88,8 +77,6 @@ class Square(Rectangle):
             "x": self.x,
             "y": self.y
         }
-
-
 
     def __str__(self):
         """Return the print() and str() representation of a Square."""
