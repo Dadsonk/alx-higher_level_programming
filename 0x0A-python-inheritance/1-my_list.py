95d2fafc61ce884151da13460cc3ac0d168841f1
Dadson Mweya <mweyadadson@gmail.com>
2023-04-26 21:29:01 +0300

    Python - Inheritance

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -3,12 +3,9 @@
 """
 
 
-
-
-
 class MyList(list):
     """Inherits from list"""
+
     def print_sorted(self):
         """Prints the list but sorted (ascending sort)"""
         print(sorted(self))
-
