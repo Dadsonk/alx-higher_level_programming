78721fdb1bc8c9152e3fa5e33f36cca2cfbfce48
Dadson Mweya <mweyadadson@gmail.com>
2023-04-27 01:01:40 +0300

    sorting_algorithms

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,32 +1,20 @@
 #!/usr/bin/python3
+""" Creating a class """
 
-'''
-Module for Rectangle class.
-Contains the class BaseGeometry and subclass Rectangle
-'''
 
 
 Rectangle = __import__('9-rectangle').Rectangle
 
 
 
-
 class Square(Rectangle):
-  	'''
-	A subclass representing a rectangle.
-	'''
-    
+    """A Square class that inherits from Rectangle"""
 
-	def __init__(self, size):
-        '''
-	Constructor.
-	'''
+    def __init__(self, size):
         self.integer_validator("size", size)
         self.__size = size
         super().__init__(size, size)
 
-   	 def area(self):
-        '''
-	Method for area of square.
-	'''
+    def area(self):
+        """ area method """
         return self.__size ** 2
