729fb1e7a415bfaf6b6e4d1193ac6b00fc16a6b0
Dadson Mweya <mweyadadson@gmail.com>
2023-05-25 04:22:47 -0700

    0x0D-SQL_introduction

---
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -0,0 +1,6 @@
+-- displays the number of records with id = 89 in the table first_table
+-- database name will be passed as argument of mysql command
+SELECT COUNT(*) 
+FROM first_table 
+WHERE id = 89;
+
