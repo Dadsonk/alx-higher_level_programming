7f32a7a66e1e99397d417ae487cfb886752c59b8
Dadson Mweya <mweyadadson@gmail.com>
2023-05-25 04:02:44 -0700

    0x0D-SQL_introduction

---
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -0,0 +1,5 @@
+-- prints the full description of the table first_table from the database hbtn_0c_0
+-- database name will be passed as argument of mysql command
+-- DESCRIBE or EXPLAIN statements not allowed
+SHOW CREATE TABLE first_table;
+
